04a88f9a89219805f233d43034464072b15d2c45
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-05-07 10:28:13 +0300

    Update SQLQuery1.sql
    insert дз

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,29 +1,10 @@
 USE [HM]
 
--- Вставка даних у таблицю Faculties
-INSERT INTO Faculties (Id, Name)
-VALUES (3, 'Computer Science'), (4, 'Software Engineering');
-
--- Вставка даних у таблицю Departments
-INSERT INTO Departments (Id, Financing, Name, FacultyId)
-VALUES (5, 10000, 'Software Development', 3), (6, 8000, 'Data Science', 4);
-
--- Вставка даних у таблицю Groups
-INSERT INTO Groups (Id, Name, Year, DepartmentId)
-VALUES (7, 'CS-1', 1, 5), (8, 'SE-1', 1, 6);
-
--- Вставка даних у таблицю Subjects
-INSERT INTO Subjects (Id, Name)
-VALUES (9, 'Mathematics'), (10, 'Programming'), (11, 'Data Structures');
-
--- Вставка даних у таблицю Teachers
-INSERT INTO Teachers (Id, Name, Salary, Surname)
-VALUES (12, 'Dave', 2000, 'McQueen'), (13, 'Jack', 2500, 'Underhill');
-
--- Вставка даних у таблицю Lectures
-INSERT INTO Lectures (Id, DayOfWeek, LectureRoom, SubjectId, TeacherId)
-VALUES (14, 1, 'D201', 9, 12), (15, 2, 'D202', 10, 13), (16, 3, 'D203', 11, 12);
-
--- Вставка даних у таблицю GroupsLectures
-INSERT INTO GroupsLectures (Id, GroupId, LectureId)
-VALUES (17, 7, 14), (18, 7, 15), (19, 8, 16);
+-- Please replace the values with your actual data
+INSERT INTO Faculties (Id, Name) VALUES (1, 'Faculty1');
+INSERT INTO Departments (Id, Financing, Name, FacultyId) VALUES (1, 10000, 'Department1', 1);
+INSERT INTO Groups (Id, Name, Year, DepartmentId) VALUES (1, 'Group1', 1, 1);
+INSERT INTO Teachers (Id, Name, Salary, Surname) VALUES (1, 'Teacher1', 5000, 'Surname1');
+INSERT INTO Subjects (Id, Name) VALUES (1, 'Subject1');
+INSERT INTO Lectures (Id, DayOfWeek, LectureRoom, SubjectId, TeacherId) VALUES (1, 1, 'Room1', 1, 1);
+INSERT INTO GroupsLectures (Id, GroupId, LectureId) VALUES (1, 1, 1);
